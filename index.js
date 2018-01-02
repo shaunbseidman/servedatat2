99ee55e55bb6d1bc32763844101a9fe09263651b
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-01-02 10:34:20 -0700

    added servedatat2

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const app = express ()
+const port = 3333
+const data = require('./content.json')
+app.use(cors())
+
+function getId(data, id){
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id == id){
+            return data[i]
+        }
+    }
+    return null
+}
+
+app.get('/', function(req,res) {
+  res.json({data})
+})
+
+app.get("/:id", function (req, res) {
+    var record = getId(data, req.params.id);
+    if (!record){
+        res.status(404).json({
+            error: {message: "No record found!"}
+        });
+    } else {
+        res.json({data: record})
+    }
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port)
+});
