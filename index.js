d995c072fc9381e95d2496f329bab33cba2920d6
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-01-03 13:08:26 -0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express ()
-const port = 3333
+const port = process.env.PORT|| 3000
 const data = require('./content.json')
 app.use(cors())
 
